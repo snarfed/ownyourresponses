d13e27dbf063e239ca4de1b9abf0b6ca8cdcd276
Ryan Barrett <git@ryanb.org>
2018-10-31 07:42:59 -0700

    bug fix for sending post names (titles) as `b'...'`
    caused by upgrading to recent granary, webutil, etc that uses both WideUnicode and future. WideUnicode.encode('utf-8') now returns a future.types.newbytes.newbytes, which urllib2 evidently reprs instead of interpreting as a str. this downgrades that to str first.

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ Polls your social network activity and creates new posts on your web site (via
 Micropub) for public Facebook comments and likes, Instagram likes, and Twitter
 @-replies, retweets, and favorites.
 """
-
-__author__ = ['Ryan Barrett <ownyourresponses@ryanb.org>']
+from __future__ import unicode_literals
 
 import logging
 import json
 import string
 import urllib
 import urllib2
+
+from future.utils import bytes_to_native_str
 
 from granary import appengine_config
 from granary import facebook
@@ -177,7 +178,7 @@ class PollHandler(webapp2.RequestHandler):
     return rendered
 
   def urlopen(self, url, data=None, headers=None):
-    data = {key: val.encode('utf-8') for key, val in data.items()}
+    data = {key: bytes_to_native_str(val.encode('utf-8')) for key, val in data.items()}
 
     logging.info('Fetching %s with headers %s, data %s', url, headers, data)
     if headers:
