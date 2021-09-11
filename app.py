52e2db2d1b7cae9bb2f9e0d320411627ea195f81
Ryan Barrett <git@ryanb.org>
2021-09-11 15:32:22 -0700

    port to Python 3 runtime and modern granary, update tokens, etc.
    was broken for the last 9 months or so :(

--- app.py
+++ app.py
@@ -4,33 +4,35 @@ Polls your social network activity and creates new posts on your web site (via
 Micropub) for public Facebook comments and likes, Instagram likes, and Twitter
 @-replies, retweets, and favorites.
 """
-from __future__ import unicode_literals
-
 import logging
 import json
-import string
-import urllib
-import urllib2
+import urllib.error, urllib.parse, urllib.request
 
-from future.utils import bytes_to_native_str
-
-from granary import appengine_config
-from granary import facebook
-from granary import instagram
-from granary import microformats2
-from granary import source as as_source
-from granary import twitter
-from oauth_dropins import handlers
-from oauth_dropins.webutil import util
-
-from google.appengine.ext import ndb
-import webapp2
+from flask import Flask
+from google.cloud import ndb
+from granary import (
+  facebook,
+  instagram,
+  microformats2,
+  source as gr_source,
+  twitter,
+)
+from oauth_dropins.webutil import (
+    appengine_info,
+    appengine_config,
+    flask_util,
+    util,
+)
+from oauth_dropins.webutil.util import json_loads
 
 # Change this to your web site's Micropub endpoint.
 # https://indiewebcamp.com/micropub
-MICROPUB_ENDPOINT = ('http://localhost/wp-json/micropub/1.0/endpoint'
-                     if appengine_config.DEBUG else
-                     'https://snarfed.org/wp-json/micropub/1.0/endpoint')
+if appengine_config.DEBUG:
+  MICROPUB_ENDPOINT = 'http://localhost/wp-json/micropub/1.0/endpoint'
+  MICROPUB_ACCESS_TOKEN = util.read('micropub_access_token_local')
+else:
+  MICROPUB_ENDPOINT = 'https://snarfed.org/wp-json/micropub/1.0/endpoint'
+  MICROPUB_ACCESS_TOKEN = util.read('micropub_access_token')
 
 # ActivityStreams objectTypes and verbs to create posts for. You can add or
 # remove types here to control what gets posted to your site.
@@ -47,11 +49,26 @@ CATEGORIES = {
   'rsvp-maybe': 'rsvp',
 }
 
-FACEBOOK_ACCESS_TOKEN = appengine_config.read('facebook_access_token')
-INSTAGRAM_ACCESS_TOKEN = appengine_config.read('instagram_access_token')
-TWITTER_ACCESS_TOKEN = appengine_config.read('twitter_access_token')
-TWITTER_ACCESS_TOKEN_SECRET = appengine_config.read('twitter_access_token_secret')
-MICROPUB_ACCESS_TOKEN = appengine_config.read('micropub_access_token')
+FACEBOOK_ACCESS_TOKEN = util.read('facebook_access_token')
+INSTAGRAM_ACCESS_TOKEN = util.read('instagram_access_token')
+TWITTER_ACCESS_TOKEN = util.read('twitter_access_token')
+TWITTER_ACCESS_TOKEN_SECRET = util.read('twitter_access_token_secret')
+TWITTER_SCRAPE_HEADERS = json_loads(util.read('twitter_scrape_headers.schnarfed.json'))
+
+
+# Flask app
+app = Flask('bridgy-fed')
+app.template_folder = './templates'
+app.config.from_mapping(
+    ENV='development' if appengine_info.DEBUG else 'PRODUCTION',
+    CACHE_TYPE='SimpleCache',
+    SECRET_KEY=util.read('flask_secret_key'),
+    JSONIFY_PRETTYPRINT_REGULAR=True,
+)
+app.register_error_handler(Exception, flask_util.handle_exception)
+
+app.wsgi_app = flask_util.ndb_context_middleware(
+    app.wsgi_app, client=appengine_config.ndb_client)
 
 
 class Response(ndb.Model):
@@ -64,130 +81,116 @@ class Response(ndb.Model):
   updated = ndb.DateTimeProperty(auto_now=True)
 
 
-class PollHandler(webapp2.RequestHandler):
+@app.route('/cron/poll')
+def poll():
   """Poll handler for cron job."""
+  # if FACEBOOK_ACCESS_TOKEN:
+  #   sources.append(facebook.Facebook(FACEBOOK_ACCESS_TOKEN))
+  # if INSTAGRAM_ACCESS_TOKEN:
+  #   sources.append(instagram.Instagram(INSTAGRAM_ACCESS_TOKEN))
+  source = twitter.Twitter(TWITTER_ACCESS_TOKEN,
+                           TWITTER_ACCESS_TOKEN_SECRET,
+                           scrape_headers=TWITTER_SCRAPE_HEADERS)
 
-  def get(self):
-    sources = []
-    # if FACEBOOK_ACCESS_TOKEN:
-    #   sources.append(facebook.Facebook(FACEBOOK_ACCESS_TOKEN))
-    # if INSTAGRAM_ACCESS_TOKEN:
-    #   sources.append(instagram.Instagram(INSTAGRAM_ACCESS_TOKEN))
-    if TWITTER_ACCESS_TOKEN:
-      sources.append(twitter.Twitter(TWITTER_ACCESS_TOKEN,
-                                     TWITTER_ACCESS_TOKEN_SECRET))
+  activities = source.get_activities(group_id=gr_source.SELF, fetch_likes=True)
+  resps = ndb.get_multi(ndb.Key('Response', util.trim_nulls(a['id']))
+                        for a in activities)
+  resps = {r.key.id(): r for r in resps if r}
 
-    for source in sources:
-      self.poll(source)
+  exception = None
+  for activity in activities:
+    obj = activity.get('object', {})
 
-  def poll(self, source):
-    activities = source.get_activities(group_id=as_source.SELF, fetch_likes=True)
-    resps = ndb.get_multi(ndb.Key('Response', util.trim_nulls(a['id']))
-                          for a in activities)
-    resps = {r.key.id(): r for r in resps if r}
+    # have we already posted or started on this response?
+    resp = resps.get(activity['id'])
+    mf2 = microformats2.object_to_json(activity)
+    mf2_props = microformats2.first_props(mf2.get('properties', {}))
+    type = gr_source.object_type(activity)
 
-    exception = None
-    for activity in activities:
-      obj = activity.get('object', {})
+    if mf2_props.get('in-reply-to'):
+      type = 'comment'  # twitter reply
+    if type not in TYPES or (resp and resp.status == 'complete'):
+      continue
+    elif resp:
+      logging.info('Retrying %s', resp)
+    else:
+      resp = Response.get_or_insert(activity['id'],
+                                    activity_json=json.dumps(activity))
+      logging.info('Created new Response: %s', resp)
 
-      # have we already posted or started on this response?
-      resp = resps.get(activity['id'])
-      mf2 = microformats2.object_to_json(activity)
-      mf2_props = microformats2.first_props(mf2.get('properties', {}))
-      type = as_source.object_type(activity)
+    base_id = source.base_object(activity)['id']
+    base = source.get_activities(activity_id=base_id)[0]
+    # logging.info(json.dumps(base, indent=2))
 
-      if mf2_props.get('in-reply-to'):
-        type = 'comment'  # twitter reply
-      if type not in TYPES or (resp and resp.status == 'complete'):
-        continue
-      elif resp:
-        logging.info('Retrying %s', resp)
-      else:
-        resp = Response.get_or_insert(activity['id'],
-                                      activity_json=json.dumps(activity))
-        logging.info('Created new Response: %s', resp)
+    # make micropub call to create post
+    # http://indiewebcamp.com/micropub
+    #
+    # include access token in both header and post body for compatibility
+    # with servers that only support one or the other (for whatever reason).
+    headers = {'Authorization': 'Bearer ' + MICROPUB_ACCESS_TOKEN}
+    data = {
+      'access_token': MICROPUB_ACCESS_TOKEN,
+      'h': 'entry',
+      'category[]': CATEGORIES.get(type),
+      'content[html]': render(source, activity, base),
+      'name': base.get('content') or base.get('object', {}).get('content'),
+    }
+    for key in 'in-reply-to', 'like-of', 'repost-of', 'published', 'updated':
+      val = mf2_props.get(key)
+      if val:
+        data[key] = microformats2.get_string_urls([val])[0]
 
-      base_id = source.base_object(activity)['id']
-      base = source.get_activities(activity_id=base_id)[0]
-      # logging.info(json.dumps(base, indent=2))
+    try:
+      result = urlopen(MICROPUB_ENDPOINT, util.trim_nulls(data), headers=headers)
+    except urllib.error.HTTPError as exception:
+      logging.exception('%s %s', exception.reason, exception.read())
+      continue
+    except urllib.error.URLError as exception:
+      logging.exception(exception.reason)
+      continue
 
-      # make micropub call to create post
-      # http://indiewebcamp.com/micropub
-      #
-      # include access token in both header and post body for compatibility
-      # with servers that only support one or the other (for whatever reason).
-      headers = {'Authorization': 'Bearer ' + MICROPUB_ACCESS_TOKEN}
-      data = {
-        'access_token': MICROPUB_ACCESS_TOKEN,
-        'h': 'entry',
-        'category[]': CATEGORIES.get(type),
-        'content[html]': self.render(source, activity, base),
-        'name': base.get('content') or base.get('object', {}).get('content'),
-      }
-      for key in 'in-reply-to', 'like-of', 'repost-of', 'published', 'updated':
-        val = mf2_props.get(key)
-        if val:
-          data[key] = microformats2.get_string_urls([val])[0]
+    resp.post_url = result.info().get('Location')
+    logging.info('Created new post: %s', resp.post_url)
+    resp.response_body = result.read()
+    logging.info('Response body: %s', resp.response_body)
 
-      try:
-        result = self.urlopen(MICROPUB_ENDPOINT, headers=headers,
-                              data=util.trim_nulls(data))
-      except urllib2.HTTPError as exception:
-        logging.exception('%s %s', exception.reason, exception.read())
-        continue
-      except urllib2.URLError as exception:
-        logging.exception(exception.reason)
-        continue
+    resp.status = 'complete'
+    resp.put()
 
-      resp.post_url = result.info().get('Location')
-      logging.info('Created new post: %s', resp.post_url)
-      resp.response_body = result.read()
-      logging.info('Response body: %s', resp.response_body)
+    # uncomment for testing
+    # return
 
-      resp.status = 'complete'
-      resp.put()
+  # end loop over activities
+  return ('Failed, see logs', 500) if exception else 'OK'
 
-      # uncomment for testing
-      # return
+def render(source, activity, base):
+  obj = activity.get('object') or activity
+  content = microformats2.render_content(obj)
+  embed = source.embed_post(base)
 
-    # end loop over activities
-    assert exception is None
+  type = gr_source.object_type(activity)
+  content = activity.get('content', '')
+  if type == 'share' and not content:
+    content = 'retweeted this.'
 
-  @staticmethod
-  def render(source, activity, base):
-    obj = activity.get('object') or activity
-    content = microformats2.render_content(obj)
-    embed = source.embed_post(base)
+  rendered = embed + content if type == 'comment' else content + embed
 
-    type = as_source.object_type(activity)
-    content = activity.get('content', '')
-    if type == 'share' and not content:
-      content = 'retweeted this.'
-
-    rendered = embed + content if type == 'comment' else content + embed
-
-    mf2_class = {'like': 'u-like-of',
-                 'share': 'u-repost-of',
-                 }.get(type, 'in-reply-to')
-    url = (obj.get('inReplyTo') or [{}])[0].get('url') or base.get('url')
-    rendered += """
+  mf2_class = {'like': 'u-like-of',
+               'share': 'u-repost-of',
+               }.get(type, 'in-reply-to')
+  url = (obj.get('inReplyTo') or [{}])[0].get('url') or base.get('url')
+  rendered += """
 <a class="%s" href="%s"></a>
 <a class="u-syndication" href="%s"></a>
 """ % (mf2_class, url, activity.get('url'))
 
-    return rendered
+  return rendered
 
-  def urlopen(self, url, data=None, headers=None):
-    data = {key: bytes_to_native_str(val.encode('utf-8')) for key, val in data.items()}
+def urlopen(url, data, headers=None):
+  data = {key: val for key, val in data.items()}
+  data = urllib.parse.urlencode(data).encode()
 
-    logging.info('Fetching %s with headers %s, data %s', url, headers, data)
-    if headers:
-      url = urllib2.Request(url, headers=headers)
-    if data:
-      data = urllib.urlencode(data)
-    return urllib2.urlopen(url, timeout=600, data=data)
-
-
-application = webapp2.WSGIApplication(
-  [('/cron/poll', PollHandler),
-   ], debug=False)
+  logging.info('Fetching %s with headers %s, data %s', url, headers, data)
+  if headers:
+    url = urllib.request.Request(url, data=data, headers=headers)
+  return urllib.request.urlopen(url, timeout=600, data=data)
--- appengine_config.py
+++
@@ -1,4 +0,0 @@
-# Load packages from virtualenv
-# https://cloud.google.com/appengine/docs/python/tools/libraries27#vendoring
-from google.appengine.ext import vendor
-vendor.add('local')
